8d280fb35fe1192b6fe34dda174d22ec04c46dc7
abir <abirchandrabhushan@gmail.com>
2019-06-27 13:42:00 +0530

    Working advanced repository search

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@
   function basicSearch(e) {
     let searchKey = document.getElementById("searchKey").value;
     storeSearches(searchKey)
-    searchKey = (searchKey.toString().toLowerCase()).trim();
+    searchKey = searchKey.toString().trim();
     let searchURL = 'https://github.com/search?q=' + searchKey;
     let createProperties = {
       "url": searchURL
@@ -35,16 +35,23 @@
     let searchKey = document.getElementById("advanced_search").value;
     let searchType = document.getElementById("search_type").value
     if (searchKey) {
-      searchKey = (searchKey.toString().toLowerCase()).trim();
+      searchKey = searchKey.toString().trim();
     }
     if (searchType == 'Packages') {
       searchType = 'RegistryPackages';
     }
-    let searchURL = 'https://github.com/search?utf8=%E2%9C%93&q=' + searchKey + '&type=' + searchType + '&ref=advsearch&l=&l=';
+    let searchParameters = getParameters();
+    let searchURL = 'https://github.com/search?utf8=%E2%9C%93&q=' + searchKey + searchParameters + '&type=' + searchType + '&ref=advsearch&l=&l=';
     let createProperties = {
       "url": searchURL
     };
     chrome.tabs.create(createProperties, function() {});
+  }
+
+  function getParameters() {
+    let param = '';
+    param += repoParams(); // + codeParams() + issueParams() + userParams() + wikiParams();
+    return param;
   }
 
   function displaySearchHistory() {
@@ -72,5 +79,48 @@
         previousSearches.pop();
       }
       localStorage["previousSearches"] = JSON.stringify(previousSearches);
+    }
+  }
+
+  function repoParams() {
+    let repoParams = '';
+    let users = document.getElementById("user").value;
+    let repo = document.getElementById("repo").value;
+    let stars = document.getElementById("stars").value;
+    let forks = document.getElementById("forks").value;
+    let size = document.getElementById("repo_size").value;
+    let fork = filterForkOption(document.getElementById("repo_fork").value);
+
+    if (!(isEmpty(users))) {
+      repoParams += '+user%3A' + users;
+    }
+    if (!(isEmpty(repo))) {
+      repoParams += '+repo%3A' + repo;
+    }
+    if (!(isEmpty(stars))) {
+      repoParams += '+stars%3A' + stars;
+    }
+    if (!(isEmpty(forks))) {
+      repoParams += '+forks%3A' + forks;
+    }
+    if (!(isEmpty(size))) {
+      repoParams += '+size%3A' + size;
+    }
+
+    return repoParams + fork;
+
+  }
+
+  function isEmpty(str) {
+    return (str == null || (str.toString().trim()).length == 0);
+  }
+
+  function filterForkOption(str) {
+    if (str == 'Yes') {
+      return '+fork%3Atrue'
+    } else if (str == 'Only including forks') {
+      return '+fork%3Aonly'
+    } else {
+      return '';
     }
   }
