d988cd3101b970c14ab3e4240a13358e475fd347
abir <abirchandrabhushan@gmail.com>
2020-02-27 18:07:08 +0000

    1.0.3

--- src/js/index.js
+++ src/js/index.js
@@ -1,272 +1,250 @@
-  $(document).ready(function() {
-    displaySearchHistory();
-    displayTip();
+import {
+  displayFact
+} from './static/facts.js'
+
+$(document).ready(function() {
+  displaySearchHistory();
+  displayFact();
+});
+
+var previousSearches = [];
+
+var basicSearch_input = document.getElementById("searchKey");
+if (basicSearch_input) {
+  basicSearch_input.addEventListener("keydown", function(e) {
+    if (e.keyCode === 13) {
+      basicSearch(e);
+    }
   });
+}
 
-  var previousSearches = [];
-  var tips = [
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;Use Advanced for constructing powerful search queries",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;52% of the Fortune 50 companies use Github Enterprise to develop software",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;Search GitHub is an Open Source project, created with &#x2764; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Contribute <a href=\"https://github.com/abircb/search-github-crx\" target=\"_blank\">here</a>",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;On March 2017, Github became the biggest Open Source host in history",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;Github has over 800 employees, of which 65% are remote",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;Github Desktop makes life easy",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;Let&apos;s say you&apos;re creating a pull request that fixes issue #1337. Then, entering the text &#34;fixes #1337&#34; in the description of your PR automagically closes that issue. Neat.",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;GitHub&apos;s mascot is a female cat with 5 octopus-like arms. It was created by Simon Oxley, who also designed Twitter&apos;s bird",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;GitHub has users in over 200 countries",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;Open Source projects on GitHub are written in over 337 programming languages, of which JavaScript is most popular",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;Sometimes emojis are worth a thousand words. The <i>thumbs up</i> emoji has been used more than 7.2 million times on Github",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;On February 28, 2018, GitHub fell victim to the second largest DDoS attack in history, with incoming traffic reaching a peak of about 1.35 terabits per second",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;Search GitHub is an Open Source project, created with &#x2764; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Contribute <a href=\"https://github.com/abircb/search-github-crx\" target=\"_blank\">here</a>",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;On June 4, 2018, Micorsoft acquired GitHub for $7.5 billion",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;GitHub offers unlimited private repositories to all plans, including free accounts!",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;GitHub was developed by Chris Wanstrath, P. J. Hyett, Tom Preston-Werner and Scott Chacon using Ruby on Rails",
-    "<i class=\"fab fa-github-alt fa-lg\"></i>&nbsp;Check out <a href=\"https://gist.github.com/\" target=\"_blank\">Gist</a> by GitHub, a pastebin-style site used for hosting code snippets"
-  ];
+var searchBtn = document.getElementById("advanced_btn");
+if (searchBtn) {
+  searchBtn.addEventListener('click', function() {
+    advancedSearch();
+  });
+}
 
-  var basicSearch_input = document.getElementById("searchKey");
-  if (basicSearch_input) {
-    basicSearch_input.addEventListener("keydown", function(e) {
-      if (e.keyCode === 13) {
-        basicSearch(e);
-      }
-    });
-  }
+function basicSearch(e) {
+  let searchKey = document.getElementById("searchKey").value;
+  storeSearches(searchKey)
+  searchKey = searchKey.toString().trim();
+  let searchURL = 'https://github.com/search?q=' + searchKey;
+  let createProperties = {
+    "url": searchURL
+  };
+  chrome.tabs.create(createProperties, function() {});
+}
 
-  var searchBtn = document.getElementById("advanced_btn");
-  if (searchBtn) {
-    searchBtn.addEventListener('click', function() {
-      advancedSearch();
-    });
-  }
+function advancedSearch() {
+  let searchKey = document.getElementById("advanced_search").value;
+  let searchType = document.getElementById("search_type").value
 
-  function basicSearch(e) {
-    let searchKey = document.getElementById("searchKey").value;
-    storeSearches(searchKey)
+  if (searchKey) {
     searchKey = searchKey.toString().trim();
-    let searchURL = 'https://github.com/search?q=' + searchKey;
-    let createProperties = {
-      "url": searchURL
-    };
-    chrome.tabs.create(createProperties, function() {});
+  } else {
+    searchKey = '';
   }
 
-  function advancedSearch() {
-    let searchKey = document.getElementById("advanced_search").value;
-    let searchType = document.getElementById("search_type").value
-
-    if (searchKey) {
-      searchKey = searchKey.toString().trim();
-    } else {
-      searchKey = '';
-    }
-
-    if (searchType == 'Packages') {
-      searchType = 'RegistryPackages';
-    }
-
-    let searchParameters = getParameters();
-    let forkOptions = getForkOptions();
-    let searchURL = 'https://github.com/search?utf8=%E2%9C%93&q=' + searchKey + searchParameters + forkOptions + '&type=' + searchType + '&ref=advsearch&l=&l=';
-    let createProperties = {
-      "url": searchURL
-    };
-    chrome.tabs.create(createProperties, function() {});
+  if (searchType == 'Packages') {
+    searchType = 'RegistryPackages';
   }
 
-  function getParameters() {
-    let param = '';
-    param += repoParameters() + codeParameters() + issueParameters() + userParameters() + wikiParameter();
-    return param;
+  let searchParameters = getParameters();
+  let forkOptions = getForkOptions();
+  let searchURL = 'https://github.com/search?utf8=%E2%9C%93&q=' + searchKey + searchParameters + forkOptions + '&type=' + searchType + '&ref=advsearch&l=&l=';
+  let createProperties = {
+    "url": searchURL
+  };
+  chrome.tabs.create(createProperties, function() {});
+}
+
+function getParameters() {
+  let param = '';
+  param += repoParameters() + codeParameters() + issueParameters() + userParameters() + wikiParameter();
+  return param;
+}
+
+function repoParameters() {
+  let repoParameters = '';
+  let users = document.getElementById("user").value;
+  let repo = document.getElementById("repo").value;
+  let stars = document.getElementById("stars").value;
+  let forks = document.getElementById("forks").value;
+  let size = document.getElementById("repo_size").value;
+
+  if (!(isEmpty(users))) {
+    repoParameters += parse('+user%3A', users);
+  }
+  if (!(isEmpty(repo))) {
+    repoParameters += parse('+repo%3A', repo);
+  }
+  if (!(isEmpty(stars))) {
+    repoParameters += parse('+stars%3A', stars);
+  }
+  if (!(isEmpty(forks))) {
+    repoParameters += parse('+forks%3A', forks);
+  }
+  if (!(isEmpty(size))) {
+    repoParameters += parse('+size%3A', size);
   }
 
-  function repoParameters() {
-    let repoParameters = '';
-    let users = document.getElementById("user").value;
-    let repo = document.getElementById("repo").value;
-    let stars = document.getElementById("stars").value;
-    let forks = document.getElementById("forks").value;
-    let size = document.getElementById("repo_size").value;
+  return repoParameters;
+}
 
-    if (!(isEmpty(users))) {
-      repoParameters += parse('+user%3A', users);
-    }
-    if (!(isEmpty(repo))) {
-      repoParameters += parse('+repo%3A', repo);
-    }
-    if (!(isEmpty(stars))) {
-      repoParameters += parse('+stars%3A', stars);
-    }
-    if (!(isEmpty(forks))) {
-      repoParameters += parse('+forks%3A', forks);
-    }
-    if (!(isEmpty(size))) {
-      repoParameters += parse('+size%3A', size);
-    }
+function codeParameters() {
+  let codeParameters = '';
+  let extension = document.getElementById("extension").value;
+  let size = document.getElementById("code_size").value;
+  let path = document.getElementById("path").value;
+  let filename = document.getElementById("filename").value;
 
-    return repoParameters;
+  if (!(isEmpty(extension))) {
+    codeParameters += parse('+extension%3A', extension);
+  }
+  if (!(isEmpty(size))) {
+    codeParameters += parse('+size%3A', size);
+  }
+  if (!(isEmpty(path))) {
+    codeParameters += parse('+path%3A', path);
+  }
+  if (!(isEmpty(filename))) {
+    codeParameters += parse('+filename%3A', filename);
   }
 
-  function codeParameters() {
-    let codeParameters = '';
-    let extension = document.getElementById("extension").value;
-    let size = document.getElementById("code_size").value;
-    let path = document.getElementById("path").value;
-    let filename = document.getElementById("filename").value;
+  return codeParameters;
+}
 
-    if (!(isEmpty(extension))) {
-      codeParameters += parse('+extension%3A', extension);
-    }
-    if (!(isEmpty(size))) {
-      codeParameters += parse('+size%3A', size);
-    }
-    if (!(isEmpty(path))) {
-      codeParameters += parse('+path%3A', path);
-    }
-    if (!(isEmpty(filename))) {
-      codeParameters += parse('+filename%3A', filename);
-    }
+function issueParameters() {
+  let issueParameters = '';
+  let comments = document.getElementById("commments").value;
+  let label = document.getElementById("label").value;
+  let author = document.getElementById("author").value;
+  let mentions = document.getElementById("mentions").value;
+  let assignee = document.getElementById("assignee").value;
 
-    return codeParameters;
+  if (!(isEmpty(comments))) {
+    issueParameters += parse('+comments%3A', comments);
+  }
+  if (!(isEmpty(label))) {
+    issueParameters += parse('+label%3A', label);
+  }
+  if (!(isEmpty(author))) {
+    issueParameters += parse('+author%3A', author);
+  }
+  if (!(isEmpty(mentions))) {
+    issueParameters += parse('+mentions%3A', mentions);
+  }
+  if (!(isEmpty(assignee))) {
+    issueParameters += parse('+assignee%3A', assignee);
   }
 
-  function issueParameters() {
-    let issueParameters = '';
-    let comments = document.getElementById("commments").value;
-    let label = document.getElementById("label").value;
-    let author = document.getElementById("author").value;
-    let mentions = document.getElementById("mentions").value;
-    let assignee = document.getElementById("assignee").value;
+  return issueParameters;
+}
 
-    if (!(isEmpty(comments))) {
-      issueParameters += parse('+comments%3A', comments);
-    }
-    if (!(isEmpty(label))) {
-      issueParameters += parse('+label%3A', label);
-    }
-    if (!(isEmpty(author))) {
-      issueParameters += parse('+author%3A', author);
-    }
-    if (!(isEmpty(mentions))) {
-      issueParameters += parse('+mentions%3A', mentions);
-    }
-    if (!(isEmpty(assignee))) {
-      issueParameters += parse('+assignee%3A', assignee);
-    }
+function userParameters() {
+  let userParameters = '';
+  let fullname = document.getElementById("fullname").value;
+  let location = document.getElementById("location").value;
+  let followers = document.getElementById("followers").value;
+  let public_repos = document.getElementById("repos").value;
 
-    return issueParameters;
+  if (!(isEmpty(fullname))) {
+    userParameters += parse('+fullname%3A', fullname);
+  }
+  if (!(isEmpty(location))) {
+    userParameters += parse('+location%3A', location);
+  }
+  if (!(isEmpty(followers))) {
+    userParameters += parse('+followers%3A', followers);
+  }
+  if (!(isEmpty(public_repos))) {
+    userParameters += parse('+repos%3A', public_repos);
   }
 
-  function userParameters() {
-    let userParameters = '';
-    let fullname = document.getElementById("fullname").value;
-    let location = document.getElementById("location").value;
-    let followers = document.getElementById("followers").value;
-    let public_repos = document.getElementById("repos").value;
+  return userParameters;
+}
 
-    if (!(isEmpty(fullname))) {
-      userParameters += parse('+fullname%3A', fullname);
-    }
-    if (!(isEmpty(location))) {
-      userParameters += parse('+location%3A', location);
-    }
-    if (!(isEmpty(followers))) {
-      userParameters += parse('+followers%3A', followers);
-    }
-    if (!(isEmpty(public_repos))) {
-      userParameters += parse('+repos%3A', public_repos);
-    }
+function wikiParameter() {
+  let wikiParameter = '';
+  let updated = document.getElementById("updated").value;
 
-    return userParameters;
+  if (!(isEmpty(updated))) {
+    wikiParameter += parse('+updated%3A', updated);
   }
 
-  function wikiParameter() {
-    let wikiParameter = '';
-    let updated = document.getElementById("updated").value;
+  return wikiParameter;
+}
 
-    if (!(isEmpty(updated))) {
-      wikiParameter += parse('+updated%3A', updated);
+function isEmpty(str) {
+  return (str == null || (str.toString().trim()).length == 0);
+}
+
+function getForkOptions() {
+  let repo_fork = filterForkOption(document.getElementById("repo_fork").value);
+  let code_fork = filterForkOption(document.getElementById("code_fork").value);
+  let state = filterState(document.getElementById("state").value);
+  return repo_fork + code_fork + state;
+}
+
+function filterForkOption(str) {
+  if (str == 'Yes') {
+    return '+fork%3Atrue'
+  } else if (str == 'Only including forks') {
+    return '+fork%3Aonly'
+  } else {
+    return '';
+  }
+}
+
+function filterState(str) {
+  if (str == 'open') {
+    return '+state%3Aopen'
+  } else if (str == 'closed') {
+    return '+state%3Aclosed'
+  } else {
+    return '';
+  }
+}
+
+function displaySearchHistory() {
+  if (localStorage["previousSearches"]) {
+    previousSearches = JSON.parse(localStorage["previousSearches"]);
+  } else {
+    previousSearches = ['octokit/rest.js', 'atom', 'search-github-crx', 'antirez/redis', 'electron'];
+  }
+  $("#searchKey").autocomplete({
+    source: function(request, response) {
+      var results = $.ui.autocomplete.filter(previousSearches, request.term);
+      response(results.slice(0, 5));
+    },
+    messages: {
+      noResults: ''
     }
+  });
+}
 
-    return wikiParameter;
-  }
-
-  function isEmpty(str) {
-    return (str == null || (str.toString().trim()).length == 0);
-  }
-
-  function getForkOptions() {
-    let repo_fork = filterForkOption(document.getElementById("repo_fork").value);
-    let code_fork = filterForkOption(document.getElementById("code_fork").value);
-    let state = filterState(document.getElementById("state").value);
-    return repo_fork + code_fork + state;
-  }
-
-  function filterForkOption(str) {
-    if (str == 'Yes') {
-      return '+fork%3Atrue'
-    } else if (str == 'Only including forks') {
-      return '+fork%3Aonly'
-    } else {
-      return '';
+function storeSearches(search) {
+  if (previousSearches.indexOf(search) == -1) {
+    previousSearches.unshift(search);
+    if (previousSearches.length > 100) {
+      previousSearches.pop();
     }
+    localStorage["previousSearches"] = JSON.stringify(previousSearches);
   }
+}
 
-  function filterState(str) {
-    if (str == 'open') {
-      return '+state%3Aopen'
-    } else if (str == 'closed') {
-      return '+state%3Aclosed'
-    } else {
-      return '';
+function parse(param, str) {
+  if (str.includes(',')) {
+    let parsedSearch = '';
+    let keywords = str.split(',');
+    i = 0;
+    while (i < keywords.length) {
+      parsedSearch += param + (keywords[i].trim());
+      i += 1;
     }
+    return parsedSearch;
+  } else {
+    return param + str;
   }
-
-  function displaySearchHistory() {
-    if (localStorage["previousSearches"]) {
-      previousSearches = JSON.parse(localStorage["previousSearches"]);
-    } else {
-      previousSearches = ['octokit/rest.js', 'atom', 'search-github-crx', 'antirez/redis', 'electron'];
-    }
-    $("#searchKey").autocomplete({
-      source: function(request, response) {
-        var results = $.ui.autocomplete.filter(previousSearches, request.term);
-        response(results.slice(0, 5));
-      },
-      messages: {
-        noResults: ''
-      }
-    });
-  }
-
-  function storeSearches(search) {
-    if (previousSearches.indexOf(search) == -1) {
-      previousSearches.unshift(search);
-      if (previousSearches.length > 100) {
-        previousSearches.pop();
-      }
-      localStorage["previousSearches"] = JSON.stringify(previousSearches);
-    }
-  }
-
-  function parse(param, str) {
-    if (str.includes(',')) {
-      let parsedSearch = '';
-      let keywords = str.split(',');
-      i = 0;
-      while (i < keywords.length) {
-        parsedSearch += param + (keywords[i].trim());
-        i += 1;
-      }
-      return parsedSearch;
-    } else {
-      return param + str;
-    }
-  }
-
-  function displayTip() {
-    let min = 0;
-    let max = tips.length - 1;
-    let n = Math.floor(Math.random() * (+max - +min)) + +min;
-    document.getElementById("tip").innerHTML = tips[n];
-  }
+}
